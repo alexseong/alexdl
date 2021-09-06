c9ce3175b59475a083277a7256fa945da709d9f3
alexseong <alex.y.seong@gmail.com>
2021-09-06 22:19:40 +0900

    utils

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+
 def _dot_var(v, verbose=False):
 	dot_var = '{} [label="{}", color=orange, style=filled]\n'
 
@@ -16,11 +19,53 @@ def _dot_func(f):
 	dot_edge = '{} -> {}\n'
 	for x in f.inputs:
 		txt += dot_edge.format(id(x), id(f))
-	for y in f.outpus:
+	for y in f.outputs:
 		txt += dot_edge.format(id(f), id(y()))
 
 	return txt
-	
-def get_dot_graph(y, verbose=False):
-	pass
 
+def get_dot_graph(output, verbose=True):
+	txt = ''
+	funcs = []
+	seen_set = set()
+
+	def add_func(f):
+		if f not in seen_set:
+			funcs.append(f)
+			seen_set.add(f)
+
+	add_func(output.creator)
+	txt += _dot_var(output, verbose)
+
+	while funcs:
+		func = funcs.pop()
+		txt += _dot_func(func)
+		for x in func.inputs:
+			txt += _dot_var(x, verbose)
+
+			if x.creator is not None:
+				add_func(x.creator)
+
+	return 'digraph g {\n' + txt + '}'
+
+def plot_dot_graph(output, verbose=True, to_file='graph.png'):
+	dot_graph = get_dot_graph(output, verbose)
+
+	tmp_dir = os.path.join(os.path.expanduser('~'), '.alexdl')
+	if not os.path.exists(tmp_dir):
+		os.mkdir(tmp_dir)
+
+	graph_path = os.path.join(tmp_dir, 'tmp_graph.dot')
+
+	with open(graph_path, 'w') as f:
+		f.write(dot_graph)
+
+	extension = os.path.splitext(to_file)[1][1:]
+	cmd = 'dot {} -T {} -o {}'.format(graph_path, extension, to_file)
+	subprocess.run(cmd, shell=True)
+
+	try:
+		from IPython.display import Image
+		return Image(filename=to_file)
+	except:
+		pass
